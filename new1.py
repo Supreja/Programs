8b1bbff7cc4983c473a5981cd7af88b861185b95
Supreja <supreja.sudarsanam1996@gmail.com>
2017-02-25 14:10:32 +0530

    Delete new1.py

--- new1.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-url = input("Enter a website to extract from:")
-r = requests.get(url)
-data =r.text
-
-soup = BeautifulSoup(data,"lxml")
-for link in soup.findAll('a',{'class':'waves-effect'}): #to find all links: soup.findAll('a')
-    print(link.get('href'))
